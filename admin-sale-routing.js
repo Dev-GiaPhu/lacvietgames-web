330aa0b679706ec871452609362493bfb02d8a56
Dev-GiaPhu <jemnguyen114@gmail.com>
2026-08-08 03:11:42 +0700

    Default sale notifications to sale games page

---
+++ admin-sale-routing.js
@@ -0,0 +1,17 @@
+(() => {
+  const SALE_PAGE='./sale.html';
+
+  function applyDefault(){
+    const input=document.getElementById('saleAnnUrl');
+    if(input&&!input.value.trim())input.value=SALE_PAGE;
+  }
+
+  document.addEventListener('click',e=>{
+    if(e.target.closest('#newSale'))setTimeout(applyDefault,0);
+    if(e.target.closest('[data-edit-sale]'))setTimeout(applyDefault,0);
+  });
+
+  document.addEventListener('submit',e=>{
+    if(e.target?.id==='saleForm')applyDefault();
+  },true);
+})();
