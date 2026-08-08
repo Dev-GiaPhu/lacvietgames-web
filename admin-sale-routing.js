cfb2f1edbadf1ae7fb520d85f39ca04f96b38c66
Dev-GiaPhu <jemnguyen114@gmail.com>
2026-08-08 10:09:32 +0700

    Load global Admin popup layout fix

--- admin-sale-routing.js
+++ admin-sale-routing.js
@@ -1,6 +1,14 @@
 (() => {
   const SALE_PAGE='./sales.html';
 
+  if(!document.querySelector('link[data-admin-modal-fix]')){
+    const link=document.createElement('link');
+    link.rel='stylesheet';
+    link.href='./admin-modal-fix.css?v=20260808-0450-modal-layout';
+    link.dataset.adminModalFix='1';
+    document.head.appendChild(link);
+  }
+
   function applyDefault(){
     const input=document.getElementById('saleAnnUrl');
     if(input&&!input.value.trim())input.value=SALE_PAGE;
